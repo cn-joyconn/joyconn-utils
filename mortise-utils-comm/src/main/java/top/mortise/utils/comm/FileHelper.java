6853169f60fc8b12f5a8244e1abc8b4e46eb92dc
eric <eric.zsp@hotmail.com>
2018-02-24 10:34:07 +0800

    修改fileHelper

--- mortise-utils-comm/src/main/java/top/mortise/utils/comm/FileHelper.java
+++ mortise-utils-comm/src/main/java/top/mortise/utils/comm/FileHelper.java
@@ -116,6 +116,16 @@ public class FileHelper {
      * 追加文件：使用FileWriter
      */
     public static void writeFile(String fileName, String content) {
+        File file=new File(fileName);
+        if(!file.exists())
+        {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
         try {
             //打开一个写文件器，构造函数中的第二个参数true表示以追加形式写文件
             FileWriter writer = new FileWriter(fileName, false);
@@ -129,6 +139,16 @@ public class FileHelper {
      * 追加文件：使用FileWriter
      */
     public static void writeFile(String fileName, char[] content) {
+        File file=new File(fileName);
+        if(!file.exists())
+        {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
         try {
             //打开一个写文件器，构造函数中的第二个参数true表示以追加形式写文件
             FileWriter writer = new FileWriter(fileName, false);
--- mortise-utils-encrypt/src/main/java/top/mortise/utils/encrypt/MD5Util.java
+++ mortise-utils-encrypt/src/main/java/top/mortise/utils/encrypt/MD5Util.java
@@ -45,6 +45,27 @@ public class MD5Util {
         }
         return stringbuilder.toString();
     }
+    public String MD5(String s) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] bytes = md.digest(s.getBytes("utf-8"));
+            return toHex(bytes);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String toHex(byte[] bytes) {
+
+        final char[] HEX_DIGITS = "0123456789ABCDEF".toCharArray();
+        StringBuilder ret = new StringBuilder(bytes.length * 2);
+        for (int i=0; i<bytes.length; i++) {
+            ret.append(HEX_DIGITS[(bytes[i] >> 4) & 0x0f]);
+            ret.append(HEX_DIGITS[bytes[i] & 0x0f]);
+        }
+        return ret.toString();
+    }
 //    /**
 //     * 测试主函数
 //     * @param args
