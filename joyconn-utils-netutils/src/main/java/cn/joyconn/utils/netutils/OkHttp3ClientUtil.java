f4a67dafae8ac891f12f7f671f1cfa670cc01f21
eric <eric.zsp@hotmail.com>
2024-03-04 21:44:47 +0800

    failCallback

--- joyconn-utils-netutils/src/main/java/cn/joyconn/utils/netutils/OkHttp3ClientUtil.java
+++ joyconn-utils-netutils/src/main/java/cn/joyconn/utils/netutils/OkHttp3ClientUtil.java
@@ -566,6 +566,21 @@ public class OkHttp3ClientUtil {
         doAsyncPost(url, queryParams, bodyParams, headers, fileName,"", fileBytes, okHttpClient,  callback);
     }
     /**
+     * 执行一个HTTP POST请求，返回请求响应的内容
+     *
+     * @param url         请求的URL地址
+     * @param queryParams 请求的查询参数（url中的queryString参数）,可以为null
+     * @param bodyParams  请求的form参数,可以为null
+     * @param headers     请求的header,可以为null
+     * @param fileName    上传的文件名
+     * @param fileBytes   上传的文件流
+     * @param okHttpClient      OkHttpClient实现类的实例对象
+     * @return 返回请求响应
+     */
+    public static void doAsyncPost(String url, Map<String, String> queryParams, Map<String, String>  bodyParams, Map<String, String> headers, String fileName, byte[] fileBytes,OkHttpClient okHttpClient,Consumer<Response> callback,  Consumer<IOException> failCallback) {
+        doAsyncPost(url, queryParams, bodyParams, headers, fileName,"", fileBytes, okHttpClient,  callback,failCallback);
+    }
+    /**
       * 执行一个HTTP POST请求，返回请求响应的内容
       *
       * @param url         请求的URL地址
